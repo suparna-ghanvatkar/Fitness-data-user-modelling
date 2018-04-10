8c348464199360d6370ff220abacd0846e474f5c
Suparna Ghanvatkar <suparnaghanvatkar@yahoo.co.in>
2018-04-10 16:27:07 +0800

    feature geenration done!

--- raw_tx.py
+++ raw_tx.py
@@ -12,6 +12,42 @@ import numpy as np
 import math
 #import time
 
+def peak(data, time):
+    greatest = np.max(val_list_x)
+    threshold = 0.9*greatest
+    peaks = [i>threshold for i in data]
+    no_of_peaks = sum(peaks)
+    while no_of_peaks<3:
+        threshold -= 0.05
+        peaks = [i>threshold for i in data]
+        no_of_peaks = sum(peaks)
+    first = peaks.index(True)
+    prev_time = time[first]
+    prev_i = first
+    timesec = 0
+    #timesum = datetime.datetime(0,0,0,0,0)
+    timediff = []
+    #this_time = 0
+    for i in range(first, len(data)):
+        if i:
+            timesum = time[i]-prev_time
+            timesec += (i-prev_i)
+            timediff.append(timesum.total_seconds())
+            prev_time = time[i]
+            prev_i = i
+    #return sum(timediff)/no_of_peaks
+    return timesec*10/no_of_peaks
+    
+def resultant(vecx, vecy, vecz):
+    res_vec = []
+    for i in range(len(vecx)):
+        res = math.sqrt(vecx[i]**2 + vecy[i]**2 + vecz[i]**2)
+        res_vec.append(res)
+    return np.mean(res_vec)
+
+def mad(data, axis=None): #mean absolute deviation
+    return np.mean(np.absolute(data - np.mean(data, axis)), axis)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('raw',type = str, help = 'raw file location')
 parser.add_argument('tx', type =str, help = 'transformed output file')
@@ -27,33 +63,41 @@ raw_window = []
 acc = 0  #if accumulate is true, accumulate the statistical feats
 prev_usr = -1
 prev_time = -1
+prev_act = ""
 val_list_x = []
 val_list_y = []
 val_list_z = []
+timestamps = []
 for line in raw_file:
-    line = line.replace(';', ',')
+    line = line.replace(';', '')
     #print line
-    usr, act, t, x, y, z, __ = line.split(',')    
+    usr, act, t, x, y, z = line.split(',')    
     userid = int(usr)
-    time = datetime.datetime.fromtimestamp(int(t)/1000.0) 
+    try:
+        time = datetime.datetime.fromtimestamp(int(t)/1000)
+    except Exception:
+        continue
     if prev_time==-1:
         prev_time = time
     time_diff = time-prev_time
-    print time_diff
+    #print time_diff, userid
     #timee = time.gmtime(int(t))
-    if acc<200 and (prev_usr==-1 or prev_usr==userid):
+    #either file is starting to process or user is previous and no gap, if more than 15 seconds that means gap, then reset!
+    if acc<200 and (prev_usr==-1 or (prev_usr==userid and time_diff.total_seconds()<15 and act in activity_list and prev_act==act)):
         if prev_usr==-1:
             prev_usr = userid
             prev_time = time
-        elif userid==prev_usr and act in activity_list:
+            prev_act = act
+        elif userid==prev_usr:
             acc += 1
             val_list_x.append(float(x))
             val_list_y.append(float(y))
-            val_list_z.append(float(z))            
+            val_list_z.append(float(z))      
+            timestamps.append(time)
     else:
         if acc==200:
         #set of transformations
-            attribute = [0.0]*44
+            attribute = [0.0]*45
             bin_list_x = list(np.digitize(val_list_x, bins))
             bin_list_y = list(np.digitize(val_list_y, bins))
             bin_list_z = list(np.digitize(val_list_z, bins))
@@ -64,23 +108,38 @@ for line in raw_file:
             sum_x = sum(val_list_x)
             sum_y = sum(val_list_y)
             sum_z = sum(val_list_z)
-            attribute[30] = sum_x/len(val_list_x)
-            attribute[31] = sum_y/len(val_list_y)
-            attribute[32] = sum_z/len(val_list_z)
-            attribute[43] = math.sqrt((sum_x**2)+(sum_y**2)+(sum_z**2))
-            #print userid, attribute
+            attribute[30] = np.mean(val_list_x)
+            attribute[31] = np.mean(val_list_y)
+            attribute[32] = np.mean(val_list_z)
+            attribute[33] = peak(val_list_x, timestamps)
+            attribute[34] = peak(val_list_y, timestamps)
+            attribute[35] = peak(val_list_z, timestamps)
+            attribute[36] = mad(val_list_x)
+            attribute[37] = mad(val_list_y)
+            attribute[38] = mad(val_list_z)
+            attribute[39] = np.std(val_list_x)
+            attribute[40] = np.std(val_list_y)
+            attribute[41] = np.std(val_list_z)
+            attribute[42] = resultant(val_list_x, val_list_y, val_list_z)
+            attribute[43] = prev_act
+            attribute[44] = prev_time
+            print userid, attribute
             #write to transformed file
             tx_file.write(str(userid)+",")
-            for attr in attribute:
+            for attr in attribute[:-1]:
                 tx_file.write(str(attr)+",")
+            tx_file.write(str(attribute[-1]))
             tx_file.write("\n")
-        raw_input()
+        #raw_input()
         acc = 0
         val_list_x[:] = []
         val_list_y[:] = []
         val_list_z[:] = []
+        timestamps[:] = []
         prev_usr = userid
         prev_time = time
+        prev_act = act
         val_list_x.append(float(x))
         val_list_y.append(float(y))
         val_list_z.append(float(z))
+        timestamps.append(time)
