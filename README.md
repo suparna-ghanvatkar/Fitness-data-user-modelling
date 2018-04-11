4e627648cbc775fc38250b4e6b7332e253535325
Suparna <t0915635@u.nus.edu>
2018-04-11 13:44:15 +0800

    Raw files transformed

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 wisdm dataset project
+
+The users are filtered into 
+act_recog_transformed.txt is ar dataset transformed 
+unlabelled_raw_transformed in transformation of unlabelled raw
+similarly lablelled is labelled raw
--- raw_tx.py
+++ raw_tx.py
@@ -37,7 +37,7 @@ def peak(data, time):
             prev_i = i
     #return sum(timediff)/no_of_peaks
     return timesec*10/no_of_peaks
-    
+
 def resultant(vecx, vecy, vecz):
     res_vec = []
     for i in range(len(vecx)):
@@ -69,77 +69,99 @@ val_list_y = []
 val_list_z = []
 timestamps = []
 for line in raw_file:
-    line = line.replace(';', '')
-    #print line
-    usr, act, t, x, y, z = line.split(',')    
-    userid = int(usr)
-    try:
-        time = datetime.datetime.fromtimestamp(int(t)/1000)
-    except Exception:
-        continue
-    if prev_time==-1:
-        prev_time = time
-    time_diff = time-prev_time
-    #print time_diff, userid
-    #timee = time.gmtime(int(t))
-    #either file is starting to process or user is previous and no gap, if more than 15 seconds that means gap, then reset!
-    if acc<200 and (prev_usr==-1 or (prev_usr==userid and time_diff.total_seconds()<15 and act in activity_list and prev_act==act)):
-        if prev_usr==-1:
-            prev_usr = userid
-            prev_time = time
-            prev_act = act
-        elif userid==prev_usr:
-            acc += 1
-            val_list_x.append(float(x))
-            val_list_y.append(float(y))
-            val_list_z.append(float(z))      
-            timestamps.append(time)
-    else:
-        if acc==200:
-        #set of transformations
-            attribute = [0.0]*45
-            bin_list_x = list(np.digitize(val_list_x, bins))
-            bin_list_y = list(np.digitize(val_list_y, bins))
-            bin_list_z = list(np.digitize(val_list_z, bins))
-            for i in range(10):
-                attribute[i] = bin_list_x.count(i)/200
-                attribute[10+i] = bin_list_y.count(i)/200
-                attribute[20+i] = bin_list_z.count(i)/200
-            sum_x = sum(val_list_x)
-            sum_y = sum(val_list_y)
-            sum_z = sum(val_list_z)
-            attribute[30] = np.mean(val_list_x)
-            attribute[31] = np.mean(val_list_y)
-            attribute[32] = np.mean(val_list_z)
-            attribute[33] = peak(val_list_x, timestamps)
-            attribute[34] = peak(val_list_y, timestamps)
-            attribute[35] = peak(val_list_z, timestamps)
-            attribute[36] = mad(val_list_x)
-            attribute[37] = mad(val_list_y)
-            attribute[38] = mad(val_list_z)
-            attribute[39] = np.std(val_list_x)
-            attribute[40] = np.std(val_list_y)
-            attribute[41] = np.std(val_list_z)
-            attribute[42] = resultant(val_list_x, val_list_y, val_list_z)
-            attribute[43] = prev_act
-            attribute[44] = prev_time
-            print userid, attribute
-            #write to transformed file
-            tx_file.write(str(userid)+",")
-            for attr in attribute[:-1]:
-                tx_file.write(str(attr)+",")
-            tx_file.write(str(attribute[-1]))
-            tx_file.write("\n")
-        #raw_input()
-        acc = 0
-        val_list_x[:] = []
-        val_list_y[:] = []
-        val_list_z[:] = []
-        timestamps[:] = []
-        prev_usr = userid
-        prev_time = time
-        prev_act = act
-        val_list_x.append(float(x))
-        val_list_y.append(float(y))
-        val_list_z.append(float(z))
-        timestamps.append(time)
+    if line:
+        try:
+            line = line.replace(';', '\n')
+            print line
+            try:
+                usr, act, t, x, y, z = line.split(',')
+                #print type(t), t
+                if t=='0':
+                    continue
+                else:
+                    try:
+                        float(z) and float(y) and float(x)
+                    except ValueError as e:
+                        print e
+                        continue
+            except Exception as e:
+                print line
+                print e
+                continue
+            userid = int(usr)
+            try:
+                time = datetime.datetime.fromtimestamp(int(t)/1000)
+            except Exception as e:
+                print e
+                continue
+            if prev_time==-1:
+                prev_time = time
+            time_diff = time-prev_time
+            #print time_diff, userid
+            #timee = time.gmtime(int(t))
+            #either file is starting to process or user is previous and no gap, if more than 15 seconds that means gap, then reset!
+            if acc<200 and (prev_usr==-1 or (prev_usr==userid and time_diff.total_seconds()<15 and act in activity_list and prev_act==act)):
+            #if this isnt training set, then we just can club on the basis of time :)
+            #if acc<200 and (prev_usr==-1 or (prev_usr==userid and time_diff.total_seconds()<15)):
+                if prev_usr==-1:
+                    prev_usr = userid
+                    prev_time = time
+                    prev_act = act
+                elif userid==prev_usr:
+                    acc += 1
+                    val_list_x.append(float(x))
+                    val_list_y.append(float(y))
+                    val_list_z.append(float(z))
+                    timestamps.append(time)
+            else:
+                if acc==200:
+                #set of transformations
+                    attribute = [0.0]*45
+                    bin_list_x = list(np.digitize(val_list_x, bins))
+                    bin_list_y = list(np.digitize(val_list_y, bins))
+                    bin_list_z = list(np.digitize(val_list_z, bins))
+                    for i in range(10):
+                        attribute[i] = bin_list_x.count(i)/200
+                        attribute[10+i] = bin_list_y.count(i)/200
+                        attribute[20+i] = bin_list_z.count(i)/200
+                    sum_x = sum(val_list_x)
+                    sum_y = sum(val_list_y)
+                    sum_z = sum(val_list_z)
+                    attribute[30] = np.mean(val_list_x)
+                    attribute[31] = np.mean(val_list_y)
+                    attribute[32] = np.mean(val_list_z)
+                    attribute[33] = peak(val_list_x, timestamps)
+                    attribute[34] = peak(val_list_y, timestamps)
+                    attribute[35] = peak(val_list_z, timestamps)
+                    attribute[36] = mad(val_list_x)
+                    attribute[37] = mad(val_list_y)
+                    attribute[38] = mad(val_list_z)
+                    attribute[39] = np.std(val_list_x)
+                    attribute[40] = np.std(val_list_y)
+                    attribute[41] = np.std(val_list_z)
+                    attribute[42] = resultant(val_list_x, val_list_y, val_list_z)
+                    attribute[43] = prev_act
+                    attribute[44] = prev_time
+                    print userid, attribute
+                    #write to transformed file
+                    tx_file.write(str(userid)+",")
+                    for attr in attribute[:-1]:
+                        tx_file.write(str(attr)+",")
+                    tx_file.write(str(attribute[-1]))
+                    tx_file.write("\n")
+                #raw_input()
+                acc = 0
+                val_list_x[:] = []
+                val_list_y[:] = []
+                val_list_z[:] = []
+                timestamps[:] = []
+                prev_usr = userid
+                prev_time = time
+                prev_act = act
+                val_list_x.append(float(x))
+                val_list_y.append(float(y))
+                val_list_z.append(float(z))
+                timestamps.append(time)
+        except Exception as e:
+            print e
+            continue
