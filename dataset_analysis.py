b693ff3f251f4b485055e952cf338bf3c89f3a3b
Suparna Ghanvatkar <suparnaghanvatkar@yahoo.co.in>
2018-04-13 15:44:43 +0800

    get activitiy stats

---
+++ dataset_analysis.py
@@ -0,0 +1,15 @@
+import argparse
+import numpy as np
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument('data', type =str, help = 'train transformed output file')
+args = parser.parse_args()
+
+act_headers = ['user','X0','X1','X2','X3','X4','X5','X6','X7','X8','X9','Y0','Y1','Y2','Y3','Y4','Y5','Y6','Y7','Y8','Y9','Z0','Z1','Z2','Z3','Z4','Z5','Z6','Z7','Z8','Z9','XAVG','YAVG','ZAVG','XPEAK','YPEAK','ZPEAK','XABSOLDEV', 'YABSOLDEV','ZABSOLDEV','XSTANDDEV','YSTANDDEV','ZSTANDDEV','Resultant','Class', 'Time']
+dataset = pd.read_csv(args.data, header = None, names = act_headers)
+
+diff_activities = dataset.Class.unique()
+for act in diff_activities:
+    act_data = dataset.loc[dataset.Class==act]
+    print act, ":", act_data.shape[0]
