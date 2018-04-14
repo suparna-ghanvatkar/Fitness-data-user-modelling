dcf8e4032f234ce251a3b9703257950953c08059
Suparna Ghanvatkar <suparnaghanvatkar@yahoo.co.in>
2018-04-14 19:01:49 +0800

    Activity recognition labelling for unlabelled data

--- activity_recognition.py
+++ activity_recognition.py
@@ -3,10 +3,21 @@ This script takes a transformed file as input, and also the model file saved usi
 '''
 
 import argparse
+import numpy as np
+import pandas as pd
+from sklearn.externals import joblib
 
 parser = argparse.ArgumentParse()
 parser.add_argument('tx', type = str, help = 'transformed featured file input path')
 parser.add_argument('model', type = str, help = 'model file path saved using joblib')
 args = parser.parse_args()
 
+act_headers = ['user','X0','X1','X2','X3','X4','X5','X6','X7','X8','X9','Y0','Y1','Y2','Y3','Y4','Y5','Y6','Y7','Y8','Y9','Z0','Z1','Z2','Z3','Z4','Z5','Z6','Z7','Z8','Z9','XAVG','YAVG','ZAVG','XPEAK','YPEAK','ZPEAK','XABSOLDEV', 'YABSOLDEV','ZABSOLDEV','XSTANDDEV','YSTANDDEV','ZSTANDDEV','Resultant','Class', 'Time']
 
+feats = pd.read_csv(args.tx, header = None, names=act_headers)
+clf = joblib.load(args.model)
+
+pred_labels = clf.predict(feats[feats.columns[1:44]])
+feats['Class'] = list(pred_labels)
+
+pd.to_csv(args.tx+'_predicted.txt')
