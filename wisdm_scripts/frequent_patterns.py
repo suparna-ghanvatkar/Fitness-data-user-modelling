ba592cc2aa64f3181c0daf8e1e514836901e88d8
Suparna <t0915635@u.nus.edu>
2018-04-21 14:32:31 +0800

    sequence of pattern finding

--- wisdm_scripts/frequent_patterns.py
+++ wisdm_scripts/frequent_patterns.py
@@ -6,8 +6,9 @@ import argparse
 from collections import Counter
 #from pyprefixspan import pyprefixspan
 #from pymining import seqmining
-from prefixspan import PrefixSpan
+#from prefixspan import PrefixSpan
 import pickle
+from itertools import product
 from activity_plot import plot_timeline
 import pandas as pd
 import numpy as np
@@ -17,6 +18,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('act',type=str,help='csv activity user file')
 parser.add_argument('analysis',type=str,help='csv file to timeline analysis with users to be considered')
 parser.add_argument('gran',type=int,help='seconds to consider as quantum')
+parser.add_argument('len',type=int, help='length of pattern string to mine')
 args = parser.parse_args()
 
 analysis = pd.read_csv(args.analysis)
@@ -32,7 +34,7 @@ act_data['start'] = act_data['start'].dt.to_pydatetime()
 act_data['end'] = act_data['end'].dt.to_pydatetime()
 act_legend = {}
 for i,a in enumerate(activities):
-    act_legend[i] = a
+    act_legend[str(i)] = a
 legend = open(args.act+'_freq_legend.txt','w')
 legend.write(str(act_legend))
 legend.close()
@@ -47,21 +49,21 @@ for usr in users:
     for date in dates:
         date_wise = user_data.loc[user_data.date==date]
         date_wise = date_wise.sort_values(by='date')
-        if date_wise.iloc[-1]['end']-date_wise.iloc[0]['start'] > timedelta(minutes=30):
-            plot_data = date_wise[['activity','start','end']]
-            plot_data['start'] = [s.time() for s in plot_data['start']]
-            plot_data['end'] = [s.time() for s in plot_data['end']]
-            plot_timeline(plot_data,str(usr)+'_'+pd.to_datetime(date).strftime("Y-%m-%d")+'.svg')
+        #if date_wise.iloc[-1]['end']-date_wise.iloc[0]['start'] > timedelta(minutes=30):
+            #plot_data = date_wise[['activity','start','end']]
+            #plot_data['start'] = [s.time() for s in plot_data['start']]
+            #plot_data['end'] = [s.time() for s in plot_data['end']]
+            #plot_timeline(plot_data,str(usr)+'_'+pd.to_datetime(date).strftime("Y-%m-%d")+'.svg')
         seqs = date_wise.seq_no.unique()
         for seq in seqs:
             print("new seq:")
-            seq_str = []
+            seq_str = ''
             act_seq = date_wise.loc[date_wise.seq_no==seq]
             act_seq = act_seq.sort_values(by='start')
             print(act_seq[['activity','duration']])
             #start_time = datetime.combine(date.today(),act_seq.iloc[0]['start'])
             start_time = act_seq.iloc[0]['start']
-            seq_str.append(activities.index(act_seq.iloc[0]['activity']))
+            seq_str += str(activities.index(act_seq.iloc[0]['activity']))
             curr_time = start_time+quant
             row = act_seq.iloc[0]
             row_i = 0
@@ -76,10 +78,10 @@ for usr in users:
                             row = act_seq.iloc[row_i]
                             act_present.append(activities.index(act_seq.iloc[0]['activity']))
                         lab,_ = Counter(act_present).most_common(1)[0]
-                        seq_str.append(lab)
+                        seq_str += str(lab)
                     else:
                         #still in this continue with this label
-                        seq_str.append(activities.index(row['activity']))
+                        seq_str += str(activities.index(row['activity']))
                     curr_time += quant
                 acts.append(seq_str)
     #print usr,":"
@@ -90,6 +92,20 @@ for usr in users:
             f.write("%s " % item)
         f.write("\n")
     f.close()
+    #now check for count of substrings of given length:
+    f = open(str(usr)+'_freq_pats.txt','w')
+    keywords = [''.join(i) for i in product(''.join(act_legend), repeat = args.len)]
+    key_counts = dict.fromkeys(keywords,0)
+    print key_counts
+    for key in keywords:
+        for seq in acts:
+            key_counts[key] += seq.count(key)
+    key_count = key_counts.items()
+    key_count.sort(key=lambda x: x[1], reverse=True)
+    print key_counts, key_count
+    for pair in key_count:
+        print>>f, pair
+    f.close()
     '''
     p = pyprefixspan(acts)
     p.run()
@@ -97,7 +113,6 @@ for usr in users:
     print usr, p.out
     frq_seqs = seqmining.freq_seq_enum(acts, 4)
     print(sorted(frq_seqs))
-    '''
     ps = PrefixSpan(acts)
     #print(ps.frequent(3))
     ps.minlen=4
@@ -106,3 +121,4 @@ for usr in users:
     for matches,patt in freq_pats:
         all_patterns.append(patt)
 pickle.dump(all_patterns, open('wisdm_freq_patts.pickle','wb'), 2)
+    '''
